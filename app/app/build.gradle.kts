b85f8610b0d2b167794f803a39b9a5123bf162ef
MeikaMorkula <187277094+MeikaMorkula@users.noreply.github.com>
2026-04-04 19:38:08 +0300

    Viikko 4 done

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -76,6 +76,9 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.foundation.layout)
+    implementation(libs.androidx.benchmark.traceprocessor)
+    implementation(libs.androidx.material3)
 
     // Compose BOM
     val composeBom = platform(libs.androidx.compose.bom)
--- app/app/src/main/java/com/example/luontopeli/data/repository/NatureSpotRepository.kt
+++ app/app/src/main/java/com/example/luontopeli/data/repository/NatureSpotRepository.kt
@@ -1,2 +1,36 @@
 package com.example.luontopeli.data.repository
 
+import com.example.luontopeli.data.local.dao.NatureSpotDao
+import com.example.luontopeli.data.local.entity.NatureSpot
+import com.example.luontopeli.data.remote.firebase.AuthManager
+import com.example.luontopeli.data.remote.firebase.FirestoreManager
+import com.example.luontopeli.data.remote.firebase.StorageManager
+import kotlinx.coroutines.flow.Flow
+
+
+class NatureSpotRepository(
+    private val dao: NatureSpotDao,
+    private val firestoreManager: FirestoreManager,
+    private val storageManager: StorageManager,
+    private val authManager: AuthManager
+) {
+    /** Flow-virta kaikista luontolöydöistä aikajärjestyksessä (uusin ensin) */
+    val allSpots: Flow<List<NatureSpot>> = dao.getAllSpots()
+
+    val spotsWithLocation: Flow<List<NatureSpot>> = dao.getSpotsWithLocation()
+
+
+    suspend fun insertSpot(spot: NatureSpot) {
+        val spotWithUser = spot.copy(userId = authManager.currentUserId, synced = true)
+        dao.insert(spotWithUser)
+    }
+
+    suspend fun deleteSpot(spot: NatureSpot) {
+        dao.delete(spot)
+    }
+
+
+    suspend fun getUnsyncedSpots(): List<NatureSpot> {
+        return dao.getUnsyncedSpots()
+    }
+}
